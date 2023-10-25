f4a3a23662eb9140c68176e3959aeac8650a1060
Janubek <brawlsrarts@gmail.com>
2023-10-25 23:14:37 +0600

    fix

--- client/src/services/UserService.ts
+++ client/src/services/UserService.ts
@@ -6,7 +6,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const authApi = createApi({
 	reducerPath: 'authApi',
-	baseQuery: fetchBaseQuery({ baseUrl: `${process.env.SERVER_API}` }),
+	baseQuery: fetchBaseQuery({ baseUrl: `${process.env.NEXT_PUBLIC_SERVER_API}` }),
 	endpoints: build => ({
 		userRegistration: build.mutation<IResponse, ISignUp>({
 			query: (user) => ({
