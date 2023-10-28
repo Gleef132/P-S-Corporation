6a98342cd8e38a3f717289888c16eba524250752
Janubek <brawlsrarts@gmail.com>
2023-10-28 14:37:48 +0600

    fix

--- server/controllers/search-controller.js
+++ server/controllers/search-controller.js
@@ -5,7 +5,7 @@ class SearchController {
 		try {
 			const trainings = await Training.find()
 			const titles = trainings.map(t => t.title.toLowerCase())
-			res.json(titles)
+			res.json({ titles: titles })
 		} catch (e) {
 			console.log(e)
 			res.status(400).json({ message: 'Error' })
